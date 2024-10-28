3c8505624cf876ddb1c563ab72111363e0127fa5
grakovne <grakovne@gmail.com>
2024-10-28 23:46:51 +0100

    redesing settings menu

--- app/src/main/java/org/grakovne/lissen/ui/theme/Theme.kt
+++ app/src/main/java/org/grakovne/lissen/ui/theme/Theme.kt
@@ -1,9 +1,11 @@
 package org.grakovne.lissen.ui.theme
 
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import org.grakovne.lissen.common.ColorScheme
@@ -11,37 +13,49 @@ import org.grakovne.lissen.common.ColorScheme
 val backgroundColor = Color(0xFFFAFAFA)
 
 private val LightColorScheme = lightColorScheme(
-    primary = FoxOrange,
-    secondary = Dark,
-    tertiary = FoxOrange,
-    background = backgroundColor,
-    surface = backgroundColor
+        primary = FoxOrange,
+        secondary = Dark,
+        tertiary = FoxOrange,
+        background = backgroundColor,
+        surface = backgroundColor
 )
+
+private val DarkColorScheme = darkColorScheme(
+        primary = FoxOrange
+)
+
 
 @Composable
 fun LissenTheme(
         colorScheme: ColorScheme,
-    content: @Composable () -> Unit
+        content: @Composable () -> Unit
 ) {
-
-    LaunchedEffect(colorScheme) {
-        println(colorScheme)
-    }
-
-    val systemUiController = rememberSystemUiController()
-
-    systemUiController.setNavigationBarColor(
-            color = backgroundColor,
-            darkIcons = true
-    )
-
-    systemUiController.setStatusBarColor(
-            color = backgroundColor,
-            darkIcons = true
-    )
-
     MaterialTheme(
-        colorScheme = LightColorScheme,
-        content = content
+            colorScheme = colorScheme.getScheme(),
+            content = {
+                val systemUiController = rememberSystemUiController()
+                val backgroundColor = MaterialTheme.colorScheme.background
+
+                SideEffect {
+                    systemUiController.setNavigationBarColor(
+                            color = backgroundColor,
+                            darkIcons = true
+                    )
+                    systemUiController.setStatusBarColor(
+                            color = backgroundColor,
+                            darkIcons = true
+                    )
+                }
+
+                content()
+            }
     )
+}
+
+private fun ColorScheme.getScheme(): androidx.compose.material3.ColorScheme {
+   return when(this) {
+        ColorScheme.FOLLOW_SYSTEM -> LightColorScheme
+        ColorScheme.LIGHT ->LightColorScheme
+        ColorScheme.DARK -> DarkColorScheme
+    }
 }
