aba21788b653f70dcad1e8f7e7bcda942a735620
grakovne <grakovne@gmail.com>
2024-10-30 11:02:34 +0100

    search feature

--- app/src/main/java/org/grakovne/lissen/ui/screens/library/composables/SearchActionComposable.kt
+++ app/src/main/java/org/grakovne/lissen/ui/screens/library/composables/SearchActionComposable.kt
@@ -26,7 +26,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun SearchBarAction() {
+fun SearchActionComposable() {
     var navigationItemSelected by remember { mutableStateOf(false) }
 
     Row(
