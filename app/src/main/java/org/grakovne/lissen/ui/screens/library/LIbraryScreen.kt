50970b12bf9e02f49d7f63010bf8efbe0d0e53b9
grakovne <grakovne@gmail.com>
2024-10-30 11:02:34 +0100

    search feature

--- app/src/main/java/org/grakovne/lissen/ui/screens/library/LIbraryScreen.kt
+++ app/src/main/java/org/grakovne/lissen/ui/screens/library/LIbraryScreen.kt
@@ -6,34 +6,22 @@ import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.togetherWith
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.Cloud
-import androidx.compose.material.icons.outlined.CloudOff
-import androidx.compose.material.icons.outlined.MoreVert
-import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.material.pullrefresh.PullRefreshIndicator
 import androidx.compose.material.pullrefresh.pullRefresh
 import androidx.compose.material.pullrefresh.rememberPullRefreshState
-import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -47,7 +35,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
-import androidx.compose.runtime.withFrameNanos
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -60,10 +47,6 @@ import androidx.paging.LoadState
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
 import coil.ImageLoader
-import com.composables.icons.lucide.Lucide
-import com.composables.icons.lucide.Search
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.launch
@@ -75,6 +58,7 @@ import org.grakovne.lissen.ui.extensions.withMinimumTime
 import org.grakovne.lissen.ui.navigation.AppNavigationService
 import org.grakovne.lissen.ui.screens.common.RequestNotificationPermissions
 import org.grakovne.lissen.ui.screens.library.composables.BookComposable
+import org.grakovne.lissen.ui.screens.library.composables.DefaultActionComposable
 import org.grakovne.lissen.ui.screens.library.composables.MiniPlayerComposable
 import org.grakovne.lissen.ui.screens.library.composables.RecentBooksComposable
 import org.grakovne.lissen.ui.screens.library.composables.fallback.LibraryFallbackComposable
@@ -134,8 +118,6 @@ fun LibraryScreen(
         }
     }
 
-    var navigationItemSelected by remember { mutableStateOf(false) }
-
     val pullRefreshState = rememberPullRefreshState(
         refreshing = pullRefreshing,
         onRefresh = {
@@ -177,93 +159,12 @@ fun LibraryScreen(
         topBar = {
             TopAppBar(
                 actions = {
-                    IconButton(
-                        onClick = {},
-                        modifier = Modifier.offset(x = 4.dp)
-                    ) {
-                        Icon(
-                            imageVector = Lucide.Search,
-                            contentDescription = null
-                        )
-                    }
-                    IconButton(onClick = {
-                        navigationItemSelected = true
-                    }) {
-                        Icon(
-                            imageVector = Icons.Outlined.MoreVert,
-                            contentDescription = "Menu"
-                        )
-                    }
-
-                    DropdownMenu(
-                        expanded = navigationItemSelected,
-                        onDismissRequest = { navigationItemSelected = false },
-                        modifier = Modifier
-                            .background(colorScheme.background)
-                            .padding(4.dp)
-                    ) {
-                        DropdownMenuItem(
-                            leadingIcon = {
-                                Icon(
-                                    imageVector = when (cachingModelView.localCacheUsing()) {
-                                        true -> Icons.Outlined.Cloud
-                                        else -> Icons.Outlined.CloudOff
-                                    },
-                                    contentDescription = null
-                                )
-                            },
-                            text = {
-                                Text(
-                                    text = when (cachingModelView.localCacheUsing()) {
-                                        true -> stringResource(R.string.disable_offline)
-                                        else -> stringResource(R.string.enable_offline)
-                                    },
-                                    style = MaterialTheme.typography.bodyMedium,
-                                    modifier = Modifier.padding(start = 8.dp)
-                                )
-                            },
-                            onClick = {
-                                navigationItemSelected = false
-
-                                coroutineScope.launch {
-                                    withFrameNanos { }
-
-                                    CoroutineScope(Dispatchers.IO).launch {
-                                        cachingModelView.toggleCacheForce()
-                                        libraryViewModel.dropHiddenBooks()
-
-                                        refreshContent(showRefreshing = false)
-                                    }
-                                }
-                            },
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(vertical = 4.dp)
-                        )
-
-                        DropdownMenuItem(
-                            leadingIcon = {
-                                Icon(
-                                    imageVector = Icons.Outlined.Settings,
-                                    contentDescription = null
-                                )
-                            },
-                            text = {
-                                Text(
-                                    stringResource(R.string.library_screen_preferences_menu_item),
-                                    style = MaterialTheme.typography.bodyMedium,
-                                    modifier = Modifier.padding(start = 8.dp)
-                                )
-                            },
-                            onClick = {
-                                navigationItemSelected = false
-                                navController.showSettings()
-                            },
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(vertical = 4.dp)
-                        )
-                    }
+                    DefaultActionComposable(
+                        navController = navController,
+                        cachingModelView = cachingModelView,
+                        libraryViewModel = libraryViewModel,
+                        onContentRefreshing = { refreshContent(showRefreshing = false) }
+                    )
                 },
                 title = {
                     Crossfade(targetState = navBarTitle, label = "navbar_title_fade") { title ->
