2e566dc6a053ba4a7607fc2968bdb02a655ce6c1
grakovne <grakovne@gmail.com>
2024-11-06 01:58:12 +0100

    wip

--- app/src/main/java/org/grakovne/lissen/ui/screens/settings/advanced/CustomHeaderComposable.kt
+++ app/src/main/java/org/grakovne/lissen/ui/screens/settings/advanced/CustomHeaderComposable.kt
@@ -1,32 +1,25 @@
 package org.grakovne.lissen.ui.screens.settings.advanced
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.DeleteOutline
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import org.grakovne.lissen.R
@@ -38,19 +31,17 @@ fun CustomHeaderComposable(
     onChanged: (ServerCustomHeader) -> Unit,
     onDelete: (ServerCustomHeader) -> Unit
 ) {
-
     Card(
         shape = RoundedCornerShape(12.dp),
         modifier = Modifier
             .fillMaxWidth()
-            .padding(horizontal = 16.dp),
-        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+            .padding(horizontal = 16.dp)
     ) {
         Row(
             modifier = Modifier
                 .fillMaxWidth()
-                .background(colorScheme.surfaceContainer)
-                .padding(start = 16.dp, end = 0.dp, top = 16.dp, bottom = 16.dp),
+                .background(colorScheme.background)
+                .padding(start = 16.dp, end = 0.dp, top = 0.dp, bottom = 8.dp),
             verticalAlignment = Alignment.CenterVertically
         ) {
             Column(
@@ -88,5 +79,4 @@ fun CustomHeaderComposable(
         }
     }
 
-    Spacer(modifier = Modifier.height(16.dp))
 }
--- app/src/main/java/org/grakovne/lissen/ui/screens/settings/advanced/CustomHeadersSettingsScreen.kt
+++ app/src/main/java/org/grakovne/lissen/ui/screens/settings/advanced/CustomHeadersSettingsScreen.kt
@@ -3,6 +3,9 @@ package org.grakovne.lissen.ui.screens.settings.advanced
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
@@ -14,6 +17,7 @@ import androidx.compose.material.icons.outlined.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FabPosition
 import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme.colorScheme
@@ -97,6 +101,13 @@ fun CustomHeadersSettingsScreen(
                             settingsViewModel.updateCustomHeaders(updatedList)
                         }
                     )
+
+                    if (index < headers.value.size - 1) {
+                        HorizontalDivider(modifier = Modifier
+                            .height(1.dp)
+                            .padding(horizontal = 24.dp)
+                        )
+                    }
                 }
             }
         },
