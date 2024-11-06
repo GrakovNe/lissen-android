e860ffd04afd704bf96d66d2707a1316962fa457
grakovne <grakovne@gmail.com>
2024-11-06 22:12:10 +0100

    Remove room annotation processor

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -119,7 +119,6 @@ dependencies {
 
     implementation(libs.androidx.room.runtime)
     implementation(libs.androidx.room.ktx)
-    annotationProcessor(libs.androidx.room.compiler)
     ksp(libs.androidx.room.compiler)
 
     testImplementation(libs.junit)
