4deb81bfe70b215869cca9ec5fddbaa3fdcaa4c9
grakovne <grakovne@gmail.com>
2024-11-21 17:28:14 +0100

    wip

--- app/src/main/java/org/grakovne/lissen/widget/WidgetControlButton.kt
+++ app/src/main/java/org/grakovne/lissen/widget/WidgetControlButton.kt
@@ -2,6 +2,7 @@ package org.grakovne.lissen.widget
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import androidx.glance.ColorFilter
 import androidx.glance.GlanceModifier
 import androidx.glance.Image
@@ -9,6 +10,7 @@ import androidx.glance.ImageProvider
 import androidx.glance.action.Action
 import androidx.glance.action.NoRippleOverride
 import androidx.glance.action.clickable
+import androidx.glance.appwidget.cornerRadius
 import androidx.glance.layout.Alignment
 import androidx.glance.layout.Row
 import androidx.glance.layout.size
@@ -33,9 +35,9 @@ fun WidgetControlButton(
             colorFilter = ColorFilter.tint(contentColor),
             modifier = GlanceModifier
                 .size(size)
+                .cornerRadius(16.dp)
                 .clickable(
-                    onClick = onClick,
-                    rippleOverride = NoRippleOverride
+                    onClick = onClick
                 )
         )
     }
