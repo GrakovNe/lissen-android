7e4dac09def0d81a1dff50ca714c202a6e326801
grakovne <grakovne@gmail.com>
2024-11-27 22:43:33 +0100

    wip

--- app/src/main/java/org/grakovne/lissen/playback/service/PlaybackNotificationService.kt
+++ app/src/main/java/org/grakovne/lissen/playback/service/PlaybackNotificationService.kt
@@ -1,46 +1,67 @@
 package org.grakovne.lissen.playback.service
 
+import androidx.annotation.OptIn
 import androidx.media3.common.Player
+import androidx.media3.common.util.UnstableApi
 import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.exoplayer.source.SilenceMediaSource
 import org.grakovne.lissen.common.RunningComponent
 import javax.inject.Inject
 import javax.inject.Singleton
-import kotlin.math.max
-import kotlin.math.min
 
 @Singleton
+@OptIn(UnstableApi::class)
 class PlaybackNotificationService @Inject constructor(
     private val exoPlayer: ExoPlayer
 ) : RunningComponent {
 
-    private var previousIndex: Int = exoPlayer.currentMediaItemIndex
 
     override fun onCreate() {
         exoPlayer.addListener(object : Player.Listener {
+
             override fun onPositionDiscontinuity(
                 oldPosition: Player.PositionInfo,
                 newPosition: Player.PositionInfo,
                 reason: Int
             ) {
-                val currentIndex = exoPlayer.currentMediaItemIndex
+                val previousIndex = oldPosition.mediaItemIndex
+                val currentIndex = newPosition.mediaItemIndex
 
-                if (exoPlayer.currentMediaItem?.mediaId != "SilenceMediaSource") {
-                    previousIndex = currentIndex
+                if (exoPlayer.currentMediaItem?.mediaId != SilenceMediaSource::class.simpleName) {
                     return
                 }
 
                 if (currentIndex != previousIndex) {
-                    val forward = currentIndex > previousIndex || (currentIndex == 0 && previousIndex == exoPlayer.mediaItemCount - 1)
-                    previousIndex = currentIndex
-
-                    val targetIndex = when (forward) {
-                        true -> min(exoPlayer.currentMediaItemIndex + 1, exoPlayer.mediaItemCount - 1)
-                        false -> max(0, exoPlayer.currentMediaItemIndex - 1)
+                    val direction = when (currentIndex > previousIndex || (currentIndex == 0 && previousIndex == exoPlayer.mediaItemCount - 1)) {
+                        true -> Direction.FORWARD
+                        false -> Direction.BACKWARD
                     }
 
-                    exoPlayer.seekTo(targetIndex, 0)
+                    findAvailableTrackIndex(exoPlayer.currentMediaItemIndex, direction, exoPlayer)
+                        ?.let { exoPlayer.seekTo(it, 0) }
+                        ?: exoPlayer.seekTo(oldPosition.mediaItemIndex, oldPosition.positionMs)
                 }
             }
         })
     }
+
+    private fun findAvailableTrackIndex(currentItem: Int, direction: Direction, exoPlayer: ExoPlayer): Int? {
+        if (exoPlayer.getMediaItemAt(currentItem).mediaId != SilenceMediaSource::class.simpleName) {
+            return currentItem
+        }
+
+        if (currentItem == 0 || currentItem == exoPlayer.mediaItemCount - 1) {
+            return null
+        }
+
+        return when (direction) {
+            Direction.FORWARD -> findAvailableTrackIndex(currentItem + 1, direction, exoPlayer)
+            Direction.BACKWARD -> findAvailableTrackIndex(currentItem - 1, direction, exoPlayer)
+        }
+    }
+}
+
+private enum class Direction {
+    FORWARD,
+    BACKWARD
 }
