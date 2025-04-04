4722f689d972afc1cc075ac81b08bcf7ea0f49b4
grakovne <grakovne@gmail.com>
2025-04-05 01:42:00 +0200

    wip

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,8 +37,8 @@ android {
         applicationId = "org.grakovne.lissen"
         minSdk = 28
         targetSdk = 35
-        versionCode = 85
-        versionName = "1.3.7"
+        versionCode = 86
+        versionName = "1.3.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
