db7681be70c1b5b07b106dbd4447f584e8323673
grakovne <grakovne@gmail.com>
2025-05-26 21:38:01 +0200

    wip

--- app/src/main/java/org/grakovne/lissen/playback/service/PlaybackSynchronizationService.kt
+++ app/src/main/java/org/grakovne/lissen/playback/service/PlaybackSynchronizationService.kt
@@ -63,7 +63,11 @@ class PlaybackSynchronizationService
       syncJob =
         serviceScope
           .launch {
-            while (exoPlayer.isPlaying) {
+            while (
+              syncJob?.isActive == true &&
+              exoPlayer.playWhenReady &&
+              exoPlayer.playbackState != Player.STATE_ENDED
+            ) {
               val nearStart = exoPlayer.duration - exoPlayer.currentPosition < SHORT_SYNC_WINDOW
               val nearEnd = exoPlayer.currentPosition < SHORT_SYNC_WINDOW
 
@@ -168,7 +172,8 @@ class PlaybackSynchronizationService
     companion object {
       private const val TAG = "PlaybackSynchronizationService"
       private const val SYNC_INTERVAL_LONG = 30_000L
-      private const val SHORT_SYNC_WINDOW = SYNC_INTERVAL_LONG * 2 - 1 // Nyquist-Shannon sampling theorem describes why -1 is important
+      private const val SHORT_SYNC_WINDOW =
+        SYNC_INTERVAL_LONG * 2 - 1 // Nyquist-Shannon sampling theorem describes why -1 is important
 
       private const val SYNC_INTERVAL_SHORT = 1_000L
     }
