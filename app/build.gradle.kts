ae4111f20ae477933653ce1596d5093b74838cee
grakovne <grakovne@gmail.com>
2025-07-13 12:12:52 +0200

    Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,8 +40,8 @@ android {
         applicationId = "org.grakovne.lissen"
         minSdk = 28
         targetSdk = 35
-        versionCode = 10506
-        versionName = "1.5.6"
+        versionCode = 10507
+        versionName = "1.5.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
