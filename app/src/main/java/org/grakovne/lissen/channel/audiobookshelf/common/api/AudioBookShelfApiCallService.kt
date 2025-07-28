02ae1b01cc78184fce40f37b6702cb3fd96684e4
grakovne <grakovne@gmail.com>
2025-07-28 18:00:27 +0200

    https://github.com/GrakovNe/lissen-android/issues/238

--- app/src/main/java/org/grakovne/lissen/channel/audiobookshelf/common/api/AudioBookShelfApiCallService.kt
+++ app/src/main/java/org/grakovne/lissen/channel/audiobookshelf/common/api/AudioBookShelfApiCallService.kt
@@ -11,75 +11,72 @@ import javax.inject.Singleton
 
 @Singleton
 class AudioBookShelfApiCallService
-  @Inject
-  constructor(
-    private val preferences: LissenSharedPreferences,
-    private val requestHeadersProvider: RequestHeadersProvider,
-  ) {
-    private var cachedHost: String? = null
-    private var cachedToken: String? = null
-    private var cachedAccessToken: String? = null
-    private var cachedHeaders: List<ServerRequestHeader> = emptyList()
-
-    private var clientCache: AudiobookshelfApiClient? = null
-
-    suspend fun <T> makeRequest(apiCall: suspend (client: AudiobookshelfApiClient) -> Response<T>): ApiResult<T> =
-      safeApiCall {
-        apiCall.invoke(getClientInstance())
-      }
-
-    private fun getClientInstance(): AudiobookshelfApiClient {
-      val host = preferences.getHost()
-      val token = preferences.getToken()
-      val accessToken = preferences.getAccessToken()
-      val headers = requestHeadersProvider.fetchRequestHeaders()
-
-      val clientChanged = host != cachedHost || token != cachedToken || headers != cachedHeaders || accessToken != cachedAccessToken
-      val current = clientCache
-
-      return when {
-        current == null || clientChanged -> {
-          cachedHost = host
-          cachedToken = token
-          cachedAccessToken = accessToken
-          cachedHeaders = headers
-
-          createClientInstance().also { clientCache = it }
-        }
-
-        else -> current
-      }
+@Inject
+constructor(
+  private val preferences: LissenSharedPreferences,
+  private val requestHeadersProvider: RequestHeadersProvider,
+) {
+  private var cachedHost: String? = null
+  private var cachedToken: String? = null
+  private var cachedAccessToken: String? = null
+  private var cachedHeaders: List<ServerRequestHeader> = emptyList()
+  
+  private var clientCache: AudiobookshelfApiClient? = null
+  
+  suspend fun <T> makeRequest(apiCall: suspend (client: AudiobookshelfApiClient) -> Response<T>): ApiResult<T> =
+    safeApiCall {
+      apiCall.invoke(getClientInstance())
     }
-
-    private fun createClientInstance(): AudiobookshelfApiClient {
-      val host = preferences.getHost()
-      val token = preferences.getToken()
-      val accessToken = preferences.getAccessToken()
-      val headers = requestHeadersProvider.fetchRequestHeaders()
-
-      if (host.isNullOrBlank()) {
-        throw IllegalStateException("Host or token is missing")
+  
+  private fun getClientInstance(): AudiobookshelfApiClient {
+    val host = preferences.getHost()
+    val token = preferences.getToken()
+    val accessToken = preferences.getAccessToken()
+    val headers = requestHeadersProvider.fetchRequestHeaders()
+    
+    val clientChanged = isClientChanged(host, token, headers, accessToken)
+    val current = clientCache
+    
+    return when {
+      current == null || clientChanged -> {
+        cachedHost = host
+        cachedToken = token
+        cachedAccessToken = accessToken
+        cachedHeaders = headers
+        
+        createClientInstance().also { clientCache = it }
       }
-
-      return apiClient(
-        host = host,
-        token = token,
-        accessToken = accessToken,
-        headers = headers,
-      ).retrofit
-        .create(AudiobookshelfApiClient::class.java)
+      
+      else -> current
     }
-
-    private fun apiClient(
-      host: String,
-      token: String?,
-      accessToken: String?,
-      headers: List<ServerRequestHeader>,
-    ): ApiClient =
-      ApiClient(
-        host = host,
-        token = token,
-        accessToken = accessToken,
-        requestHeaders = headers,
-      )
   }
+  
+  private fun createClientInstance(): AudiobookshelfApiClient {
+    val host = preferences.getHost()
+    val token = preferences.getToken()
+    val accessToken = preferences.getAccessToken()
+    val headers = requestHeadersProvider.fetchRequestHeaders()
+    
+    if (host.isNullOrBlank()) {
+      throw IllegalStateException("Host or token is missing")
+    }
+    
+    val client = ApiClient(
+      host = host,
+      token = token,
+      accessToken = accessToken,
+      requestHeaders = headers,
+    )
+    
+    return client
+      .retrofit
+      .create(AudiobookshelfApiClient::class.java)
+  }
+  
+  private fun isClientChanged(
+    host: String?,
+    token: String?,
+    headers: List<ServerRequestHeader>,
+    accessToken: String?
+  ) = host != cachedHost || token != cachedToken || headers != cachedHeaders || accessToken != cachedAccessToken
+}
