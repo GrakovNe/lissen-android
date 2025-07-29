6de3da95a66916a27decbaaaf3dcd31735dccff5
grakovne <grakovne@gmail.com>
2025-07-29 14:16:36 +0200

    https://github.com/GrakovNe/lissen-android/issues/238

--- app/src/main/java/org/grakovne/lissen/channel/audiobookshelf/common/api/AudioBookShelfApiService.kt
+++ app/src/main/java/org/grakovne/lissen/channel/audiobookshelf/common/api/AudioBookShelfApiService.kt
@@ -1,5 +1,8 @@
 package org.grakovne.lissen.channel.audiobookshelf.common.api
 
+import android.util.Log
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 import org.grakovne.lissen.channel.audiobookshelf.common.client.AudiobookshelfApiClient
 import org.grakovne.lissen.channel.audiobookshelf.common.converter.LoginResponseConverter
 import org.grakovne.lissen.channel.common.ApiClient
@@ -28,6 +31,8 @@ class AudioBookShelfApiService
 
     private var clientCache: AudiobookshelfApiClient? = null
 
+    private val mutex = Mutex()
+
     suspend fun <T> makeRequest(apiCall: suspend (client: AudiobookshelfApiClient) -> Response<T>): ApiResult<T> {
       val callResult =
         safeApiCall {
@@ -39,6 +44,7 @@ class AudioBookShelfApiService
           when (callResult.code) {
             ApiError.Unauthorized -> {
               refreshToken()
+              
               safeApiCall {
                 apiCall.invoke(getClientInstance())
               }
@@ -52,29 +58,32 @@ class AudioBookShelfApiService
     }
 
     private suspend fun refreshToken() {
-      val currentToken = preferences.getRefreshToken() ?: return
+      mutex.withLock {
+        val currentToken = preferences.getRefreshToken() ?: return@withLock
 
-      val refreshResult =
-        currentToken
-          .let { safeApiCall { getClientInstance().refreshToken(it) } }
-          .map { response -> loginResponseConverter.apply(response) }
+        val refreshResult =
+          safeApiCall { getClientInstance().refreshToken(currentToken) }
+            .map { loginResponseConverter.apply(it) }
 
-      when (refreshResult) {
-        is ApiResult.Error<*> -> {
-          if (refreshResult.code == ApiError.Unauthorized) {
-            preferences.clearCredentials()
-          }
-        }
-
-        is ApiResult.Success<UserAccount> -> {
-          refreshResult.data.refreshToken?.let { token ->
-            cachedRefreshToken = token
-            preferences.saveRefreshToken(token)
+        when (refreshResult) {
+          is ApiResult.Error<*> -> {
+            Log.d(TAG, "Refresh token update has been failed due to: $refreshResult")
+            if (refreshResult.code == ApiError.Unauthorized) {
+              preferences.clearCredentials()
+            }
           }
 
-          refreshResult.data.accessToken?.let { token ->
-            cachedAccessToken = token
-            preferences.saveAccessToken(token)
+          is ApiResult.Success<UserAccount> -> {
+            Log.d(TAG, "Refresh token has been updated")
+
+            refreshResult.data.refreshToken?.let {
+              cachedRefreshToken = it
+              preferences.saveRefreshToken(it)
+            }
+            refreshResult.data.accessToken?.let {
+              cachedAccessToken = it
+              preferences.saveAccessToken(it)
+            }
           }
         }
       }
@@ -134,4 +143,8 @@ class AudioBookShelfApiService
       headers: List<ServerRequestHeader>,
       accessToken: String?,
     ) = host != cachedHost || token != cachedToken || headers != cachedHeaders || accessToken != cachedAccessToken
+
+    companion object {
+      private const val TAG = "AudioBookShelfApiService"
+    }
   }
