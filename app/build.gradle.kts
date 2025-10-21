db5f6451b29dc100bc3cc515a9d8f9a598183ae6
Max Grakov <grakovne@gmail.com>
2025-10-21 16:35:06 +0200

    Feature/cancel downloads (#294)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,8 +40,8 @@ android {
         applicationId = "org.grakovne.lissen"
         minSdk = 28
         targetSdk = 36
-        versionCode = 10701
-        versionName = "1.7.1"
+        versionCode = 10702
+        versionName = "1.7.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
