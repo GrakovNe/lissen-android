a11ad8684d2f9b16cce69438724807ba9b0bdb19
Max Grakov <grakovne@gmail.com>
2025-11-04 12:27:38 +0100

    Feature/replace gson (#300)

--- app/src/main/kotlin/org/grakovne/lissen/channel/audiobookshelf/common/model/auth/AuthMethodResponse.kt
+++ app/src/main/kotlin/org/grakovne/lissen/channel/audiobookshelf/common/model/auth/AuthMethodResponse.kt
@@ -1,8 +1,10 @@
 package org.grakovne.lissen.channel.audiobookshelf.common.model.auth
 
 import androidx.annotation.Keep
+import com.squareup.moshi.JsonClass
 
 @Keep
+@JsonClass(generateAdapter = true)
 data class AuthMethodResponse(
   val authMethods: List<String> = emptyList(),
 )
