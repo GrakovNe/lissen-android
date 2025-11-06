6020e94cad865441b9c58b90220b93fbe10a91c5
Max Grakov <grakovne@gmail.com>
2025-11-06 22:46:50 +0100

    Feature/custom login button (#301)
    * Show oAuth login text from the server

--- app/src/main/kotlin/org/grakovne/lissen/channel/audiobookshelf/common/model/auth/AuthMethodResponse.kt
+++ app/src/main/kotlin/org/grakovne/lissen/channel/audiobookshelf/common/model/auth/AuthMethodResponse.kt
@@ -7,4 +7,11 @@ import com.squareup.moshi.JsonClass
 @JsonClass(generateAdapter = true)
 data class AuthMethodResponse(
   val authMethods: List<String> = emptyList(),
+  val authFormData: AuthFormData?,
+)
+
+@Keep
+@JsonClass(generateAdapter = true)
+data class AuthFormData(
+  val authOpenIDButtonText: String?,
 )
