5c6de3deb1c3b9825e98e7e011e3fb1930f4e59a
Max Grakov <grakovne@gmail.com>
2026-01-12 12:14:25 +0300

    Feature/321: software audio decoding (#332)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,7 +54,7 @@ android {
     minSdk = 28
     targetSdk = 36
     versionCode = 10801
-    versionName = "1.8.1-$commitHash"
+    versionName = "1.8.2-$commitHash"
     
     buildConfigField("String", "GIT_HASH", "\"$commitHash\"")
     
@@ -127,6 +127,8 @@ dependencies {
   implementation(libs.material)
   implementation(libs.material3)
   
+  implementation(libs.androidx.media3.ffmpeg.decoder)
+  implementation(libs.process.phoenix)
   implementation(libs.androidx.material)
   implementation(libs.compose.shimmer.android)
   
