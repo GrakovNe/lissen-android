cd6c87dbfc1a27c4b8d1eeded2e01cc77b47ccbb
grakovne <grakovne@gmail.com>
2026-01-29 23:02:18 +0300

    Fix minor issue (app crash) when device can't vibrate

--- app/src/main/kotlin/org/grakovne/lissen/common/HapticAction.kt
+++ app/src/main/kotlin/org/grakovne/lissen/common/HapticAction.kt
@@ -2,11 +2,16 @@ package org.grakovne.lissen.common
 
 import android.view.HapticFeedbackConstants
 import android.view.View
+import timber.log.Timber
 
 fun withHaptic(
   view: View,
   action: () -> Unit,
 ) {
   action()
-  view.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY)
+  try {
+    view.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY)
+  } catch (ex: Exception) {
+    Timber.w(ex)
+  }
 }
