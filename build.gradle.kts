53e092f21044a9e3ff3b79245e9be1de534a60a1
grakovne <grakovne@gmail.com>
2026-02-09 13:23:26 +0300

    bump dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     alias(libs.plugins.android.application) apply false
-    id("com.google.dagger.hilt.android") version "2.59" apply false
+    id("com.google.dagger.hilt.android") version "2.59.1" apply false
     id("com.google.devtools.ksp") version "2.2.21-2.0.4" apply false
     alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.android.library) apply false
