d7f8ee8d44d5022379ddaf7fa490266eaba3eafe
grakovne <grakovne@gmail.com>
2026-02-27 22:25:28 +0300

    clean up

--- app/src/main/kotlin/org/grakovne/lissen/util/FuturesUtil.kt
+++ app/src/main/kotlin/org/grakovne/lissen/util/FuturesUtil.kt
@@ -6,12 +6,13 @@ import java.util.concurrent.CompletableFuture
 
 fun <T> CompletableFuture<T>.asListenableFuture(): ListenableFuture<T> {
   val settableFuture = SettableFuture.create<T>()
+
   whenComplete { result, error ->
-    if (error != null) {
-      settableFuture.setException(error)
-    } else {
-      settableFuture.set(result)
+    when (error) {
+      null -> settableFuture.set(result)
+      else -> settableFuture.setException(error)
     }
   }
+
   return settableFuture
 }
