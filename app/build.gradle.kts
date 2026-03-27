36bbc45afaa0b1d772903945cad9ba9527f217ad
grakovne <grakovne@gmail.com>
2026-03-27 16:48:12 +0300

    Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,8 +53,8 @@ android {
     applicationId = "org.grakovne.lissen"
     minSdk = 28
     targetSdk = 36
-    versionCode = 10900
-    versionName = "1.9.0-$commitHash"
+    versionCode = 10901
+    versionName = "1.9.1-$commitHash"
     
     buildConfigField("String", "GIT_HASH", "\"$commitHash\"")
     
