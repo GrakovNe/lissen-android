2b8735f042001621ccfe4a58b420a39b5e25e360
grakovne <grakovne@gmail.com>
2026-05-25 17:30:59 +0300

    Revert "wip"
    This reverts commit cacd00ba86714f43ab65a8880fa33ef6270734a4.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,8 +40,8 @@ android {
     applicationId = "org.grakovne.lissen"
     minSdk = 28
     targetSdk = 37
-    versionCode = 10916
-    versionName = "1.9.16-release"
+    versionCode = 10915
+    versionName = "1.9.15-release"
     
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     
--- app/src/main/kotlin/org/grakovne/lissen/channel/audiobookshelf/common/api/AudioBookshelfRepository.kt
+++ app/src/main/kotlin/org/grakovne/lissen/channel/audiobookshelf/common/api/AudioBookshelfRepository.kt
@@ -1,5 +1,7 @@
 package org.grakovne.lissen.channel.audiobookshelf.common.api
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import okio.Buffer
 import org.grakovne.lissen.channel.audiobookshelf.common.model.MediaProgressResponse
 import org.grakovne.lissen.channel.audiobookshelf.common.model.bookmark.BookmarkRequest
@@ -30,7 +32,6 @@ class AudioBookshelfRepository
   @Inject
   constructor(
     private val audioBookShelfApiService: AudioBookShelfApiService,
-    private val audiobookshelfCoverProvider: AudiobookshelfCoverProvider,
   ) {
     suspend fun fetchLibraries(): OperationResult<LibraryResponse> =
       audioBookShelfApiService
@@ -213,5 +214,18 @@ class AudioBookshelfRepository
     suspend fun fetchBookCover(
       itemId: String,
       width: Int?,
-    ): OperationResult<Buffer> = audiobookshelfCoverProvider.fetchCover(itemId, width)
+    ): OperationResult<Buffer> =
+      audioBookShelfApiService
+        .makeRequest {
+          when (width == null) {
+            true -> it.getItemCover(itemId = itemId)
+            false -> it.getItemCover(itemId = itemId, width)
+          }
+        }.map { response ->
+          withContext(Dispatchers.IO) {
+            response.use {
+              Buffer().apply { writeAll(it.source()) }
+            }
+          }
+        }
   }
