78e19ba1ea689be88ed6d480fc459a5a99c7fb7e
grakovne <grakovne@gmail.com>
2026-05-25 18:03:17 +0300

    wip

---
+++ app/src/main/kotlin/org/grakovne/lissen/playback/PlaybackEventBus.kt
@@ -0,0 +1,60 @@
+package org.grakovne.lissen.playback
+
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.trySendBlocking
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.receiveAsFlow
+import org.grakovne.lissen.lib.domain.TimerOption
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class PlaybackEventBus
+  @Inject
+  constructor() {
+    private val _events = MutableSharedFlow<PlaybackEvent>(extraBufferCapacity = 8)
+    val events: SharedFlow<PlaybackEvent> = _events.asSharedFlow()
+
+    private val _commands = Channel<PlaybackCommand>(Channel.BUFFERED)
+    val commands: Flow<PlaybackCommand> = _commands.receiveAsFlow()
+
+    fun emit(event: PlaybackEvent) {
+      _events.tryEmit(event)
+    }
+
+    fun send(command: PlaybackCommand) {
+      _commands.trySendBlocking(command)
+    }
+  }
+
+sealed class PlaybackEvent {
+  data object PlaybackReady : PlaybackEvent()
+
+  data object TimerExpired : PlaybackEvent()
+
+  data class TimerTick(
+    val remainingSeconds: Long,
+  ) : PlaybackEvent()
+}
+
+sealed class PlaybackCommand {
+  data object Play : PlaybackCommand()
+
+  data object Pause : PlaybackCommand()
+
+  data object PreparePlayback : PlaybackCommand()
+
+  data class SeekTo(
+    val position: Double,
+  ) : PlaybackCommand()
+
+  data class SetTimer(
+    val delay: Double,
+    val option: TimerOption,
+  ) : PlaybackCommand()
+
+  data object CancelTimer : PlaybackCommand()
+}
