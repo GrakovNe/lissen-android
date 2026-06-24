a51a0dcb7626fe6867b2f4bbd31c5c0dd3faafca
grakovne <grakovne@gmail.com>
2026-06-24 23:08:50 +0300

    wip

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -102,15 +102,6 @@ android {
       enableUnitTestCoverage = true
       enableAndroidTestCoverage = true
     }
-    create("benchmark") {
-      initWith(getByName("release"))
-      applicationIdSuffix = ".benchmark"
-      versionNameSuffix = " (BENCHMARK)"
-      signingConfig = signingConfigs.getByName("debug")
-      isDebuggable = false
-      isProfileable = true
-      matchingFallbacks.add("release")
-    }
   }
   
   compileOptions {
@@ -205,7 +196,6 @@ dependencies {
   implementation(libs.converter.moshi)
   implementation(libs.moshi)
   implementation(libs.zip4j)
-  implementation(libs.androidx.profileinstaller)
 
   debugImplementation(libs.androidx.ui.tooling)
   debugImplementation(libs.androidx.ui.test.manifest)
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -26,4 +26,3 @@ dependencyResolutionManagement {
 
 rootProject.name = "Lissen"
 include(":app")
-include(":macrobenchmark")
