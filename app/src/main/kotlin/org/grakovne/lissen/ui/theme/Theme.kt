5c9da074ab9e8cc7a5d12d8f6a439ae5977b6a55
grakovne <grakovne@gmail.com>
2026-07-04 01:45:56 +0300

    Fix material You colors

--- app/src/main/kotlin/org/grakovne/lissen/ui/theme/Theme.kt
+++ app/src/main/kotlin/org/grakovne/lissen/ui/theme/Theme.kt
@@ -72,9 +72,9 @@ fun LissenTheme(
     when {
       materialYouPreferred -> {
         if (isDarkTheme) {
-          dynamicDarkColorScheme(context).copy(primary = FoxOrangeDimmed)
+          dynamicDarkColorScheme(context)
         } else {
-          dynamicLightColorScheme(context).copy(primary = FoxOrange)
+          dynamicLightColorScheme(context)
         }
       }
 
