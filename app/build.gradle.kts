8778660ac9c2173afe192f6c5e1f389570b4505f
GitLab CI <gitlab-ci@local>
2026-09-23 07:14:05 +0000

    chore: bump version 1.12.5-release -> 1.12.6-release (code 11205 -> 11206)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -84,8 +84,8 @@ android {
     applicationId = "org.grakovne.lissen"
     minSdk = 28
     targetSdk = 37
-    versionCode = 11205
-    versionName = "1.12.5-release"
+    versionCode = 11206
+    versionName = "1.12.6-release"
     
     testInstrumentationRunner = "org.grakovne.lissen.HiltTestRunner"
     
